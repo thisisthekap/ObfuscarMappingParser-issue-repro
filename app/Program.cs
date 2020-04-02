0235f23f512eba4e738c5ebf66773d599bb03522
thisisthekap <c.kapplmueller@me.com>
2020-04-02 14:23:01 +0200

    initial commit

---
+++ app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using lib;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            new SomethingFancy().DoIt();
+        }
+    }
+}
---
+++ lib/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lib
+{
+    public class SomethingFancy
+    {
+        public void DoIt()
+        {
+            SomeSecretStuff("a");
+        }
+
+        private void SomeSecretStuff(string s)
+        {
+            Console.WriteLine("Das ist ein Test: " + MakeUpperCase(null));
+        }
+
+        private string MakeUpperCase(string s)
+        {
+            return s.ToUpperInvariant();
+        }
+    }
+}
